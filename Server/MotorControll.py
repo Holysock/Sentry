19320665cb61bdd32b3e994a3015296054bd5522
Holysock <nick-lemberger@live.de>
2016-05-20 21:19:06 +0000

    still trying to fix the MotorControll class

--- Server/MotorControll.py
+++ Server/MotorControll.py
@@ -2,18 +2,19 @@ import RPi.GPIO as GPIO
 
 class motor:
     pwmY,dirY,pwmX,dirX,laser = 12,11,35,32,37
+    GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)   
-    GPIO.setup(self.pwmY, GPIO.OUT)
-    GPIO.setup(self.dirY, GPIO.OUT)
-    GPIO.setup(self.dirX, GPIO.OUT)
-    GPIO.setup(self.pwmX, GPIO.OUT)
-    GPIO.setup(self.laser, GPIO.OUT)
+    GPIO.setup(pwmY, GPIO.OUT)
+    GPIO.setup(dirY, GPIO.OUT)
+    GPIO.setup(dirX, GPIO.OUT)
+    GPIO.setup(pwmX, GPIO.OUT)
+    GPIO.setup(laser, GPIO.OUT)
 
-    GPIO.output(self.dirX, False)
-    GPIO.output(self.dirY, False)
-    GPIO.output(self.laser, False)
-    pY = GPIO.PWM(self.pwmY, 8000)
-    pX = GPIO.PWM(self.pwmX, 8000)
+    GPIO.output(dirX, False)
+    GPIO.output(dirY, False)
+    GPIO.output(laser, False)
+    pY = GPIO.PWM(pwmY, 8000)
+    pX = GPIO.PWM(pwmX, 8000)
     pX.start(0)
     pY.start(0)
     
@@ -24,7 +25,7 @@ class motor:
         if value > 100: value = 100
         elif value < -100: value = -100
         if value > 0:
-            self.GPIO.output(self.dirX, False)
+            #self.GPIO.output(self.dirX, False)
             self.pX.ChangeDutyCycle(value)
         elif value < 0:
             self.GPIO.output(self.dirX, True)
